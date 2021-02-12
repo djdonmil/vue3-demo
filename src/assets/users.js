9e7973658bd241a12a8a961a272e5fa402f8043c
dhrumildev <dhrumilshah280394@gmail.com>
2021-02-12 14:32:42 +0530

    initial commit

--- src/assets/users.js
+++ src/assets/users.js
@@ -1,41 +1,47 @@
-export const users = [
-  {
-    "id": 1,
-    "username": "_MitchellRomney",
-    "firstName": "Mitchell",
-    "lastName": "Romney",
-    "email": "mitchellromney@theearthissquare.com",
-    "isAdmin": true,
-    "twoots": [
-      { "id": 1, "content": "Twotter is Amazing!" },
-      { "id": 2, "content": "Don't forget to subscriber to The Earth is Square!" }
-    ]
-  },
-  {
-    "id": 2,
-    "username": "JColeNC",
-    "firstName": "J.",
-    "lastName": "Cole",
-    "email": null,
-    "isAdmin": false,
-    "twoots": []
-  },
-  {
-    "id": 3,
-    "username": "kurtisconner",
-    "firstName": "kurtis",
-    "lastName": "conner",
-    "email": null,
-    "isAdmin": false,
-    "twoots": []
-  },
-  {
-    "id": 4,
-    "username": "boburnham",
-    "firstName": "Bo",
-    "lastName": "Burnham",
-    "email": null,
-    "isAdmin": false,
-    "twoots": []
-  }
+export const users = [{
+        "id": 1,
+        "username": "oneclick_it",
+        "firstName": "One",
+        "lastName": "Click",
+        "email": "dhrumil@itoneclick.com",
+        "isAdmin": true,
+        "twoots": [
+            { "id": 1, "content": "Oneclick is Amazing!" },
+            { "id": 2, "content": "Dhrumil is also amazing!!" }
+        ]
+    },
+    {
+        "id": 2,
+        "username": "donmil_productions",
+        "firstName": "Donmil",
+        "lastName": "Productions",
+        "email": "support@djdonmil.com",
+        "isAdmin": false,
+        "twoots": [
+            { "id": 1, "content": "Music is life!" }
+        ]
+    },
+    {
+        "id": 3,
+        "username": "apple_india",
+        "firstName": "Tim",
+        "lastName": "Cook",
+        "email": null,
+        "isAdmin": false,
+        "twoots": [
+            { "id": 1, "content": "All new 12 Pro Max!" }
+        ]
+    },
+    {
+        "id": 4,
+        "username": "Tesla",
+        "firstName": "Elon",
+        "lastName": "Musk",
+        "email": null,
+        "isAdmin": false,
+        "twoots": [
+            { "id": 1, "content": "Cyber Trucks are great" },
+            { "id": 2, "content": "My dream is Space Mission!" }
+        ]
+    }
 ];
